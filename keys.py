e8aa131742b2180329ed26a53f033cb4f58b44c2
Janithmanodya2002 <janithmanodya102@gmail.com>
2025-08-06 05:02:12 -0700

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,22 @@
+# keys.py
+
+# Testnet Futures API credentials
+api_testnet   = "7fabcde36d70d00d01f9a3d21b38855450aec5c4348d2361fac5c6bd44afd872"
+secret_testnet = "4c22d3644841e6912dd957a0dfbfd4a6475b7bb6bc3022261173de41f165949c"
+
+# (Optional) Mainnet credentials if you later want to switch environments
+api_mainnet   = "vxFwGRQPnABh0YDtPTHrTpW1hsgKRPVutr94XlbkGeR1P0QzdZNYZXU1Dz1Uud5p"
+secret_mainnet = "E2qnexdIucEmJ7ZDwbJps4jPTt9V2evuVjJYGNOvrzLKI6ZMgwjsSHppkhjIz4cD"
+
+
+# Telegram Bot Configuration
+telegram_bot_token = "8184556638:AAE4cJMUf0z7yPoXd5si12SrqV_n_2k4eeQ" # Replace with your actual bot token
+telegram_chat_id = "-1002779240134" # Replace with your actual chat ID
+
+
+
+# keys.py
+BINANCE_API_KEY = "vxFwGRQPnABh0YDtPTHrTpW1hsgKRPVutr94XlbkGeR1P0QzdZNYZXU1Dz1Uud5p"
+BINANCE_API_SECRET = "E2qnexdIucEmJ7ZDwbJps4jPTt9V2evuVjJYGNOvrzLKI6ZMgwjsSHppkhjIz4cD"
+TELEGRAM_BOT_TOKEN = "8184556638:AAE4cJMUf0z7yPoXd5si12SrqV_n_2k4eeQ"
+TELEGRAM_CHAT_ID = "-1002779240134"
